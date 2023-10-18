18507e1e67859362217465bd7f3053afbfe308c5
Sal Aurigemma, UHM <sa8@hawaii.edu>
2023-10-18 10:02:40 -1000

    POKE10 completed

--- POKE/POKE Ju/invoice3a.js
+++ POKE/POKE Ju/invoice3a.js
@@ -1,35 +1,18 @@
-// invoice.js
-// Product Data
-let item1 = 'Apple Watch Ultra screen protector';
-let quantity1 = 2;
-let price1 = 6.23;
-let extended_price1 = quantity1 * price1;
+// invoice3.js
 
-// Define additional product data and calculate extended prices for each product (item2, item3, ...).
+//import data from products.js into this file
+import { itemData, quantity } from './products.js';
 
-// Compute extended prices for other products.
-let item2 = 'Apple Watch Ultra extra band';
-let quantity2 = 1;
-let price2 = 22.64;
-let extended_price2 = quantity2 * price2;
-
-let item3 = 'iPhone 15 Pro case';
-let quantity3 = 1;
-let price3 = 25.99;
-let extended_price3 = quantity3 * price3;
-
-let item4 = 'Apple Watch charging cable';
-let quantity4 = 1;
-let price4 = 33.95;
-let extended_price4 = quantity4 * price4;
-
-let item5 = 'Apple USB-C cable';
-let quantity5 = 3;
-let price5 = 35.00;
-let extended_price5 = quantity5 * price5;
-
-// Calculate subtotal
-let subtotal = extended_price1 + extended_price2 + extended_price3+extended_price4+extended_price5;
+// Calculate extended prices for each item using a loop and increment subtotal as it goes
+let extendedPrices = [];
+let subtotal=0;
+for (let i = 0; i < itemData.length; i++) {
+  let item = itemData[i];
+  let itemQuantity = quantity[item.quantityIndex];
+  let extendedPrice = item.price * itemQuantity;
+  subtotal+=extendedPrice;
+  extendedPrices.push(extendedPrice);
+}
 
 // Calculate sales tax
 let taxRate = 0.0575; // 5.75%
@@ -40,32 +23,6 @@ let total = subtotal + taxAmount;
 
 // Populate the table rows using DOM manipulation
 let table = document.getElementById('invoiceTable');
-
-let row = table.insertRow(); // Create a new row for each item
-row.insertCell(0).innerHTML = `${item1}`;
-row.insertCell(1).innerHTML = `${quantity1}`;
-row.insertCell(2).innerHTML = '$'+`${price1}`;
-row.insertCell(3).innerHTML = ('$' + `${extended_price1}`);
-row = table.insertRow(); // Create a new row for each item
-row.insertCell(0).innerHTML = `${item2}`;
-row.insertCell(1).innerHTML = `${quantity2}`;
-row.insertCell(2).innerHTML = '$'+`${price2}`;
-row.insertCell(3).innerHTML = ('$' + `${extended_price2}`);
-row = table.insertRow(); // Create a new row for each item
-row.insertCell(0).innerHTML = `${item3}`;
-row.insertCell(1).innerHTML = `${quantity3}`;
-row.insertCell(2).innerHTML = '$'+`${price3}`;
-row.insertCell(3).innerHTML = ('$' + `${extended_price3}`);
-row = table.insertRow(); // Create a new row for each item
-row.insertCell(0).innerHTML = `${item4}`;
-row.insertCell(1).innerHTML = `${quantity4}`;
-row.insertCell(2).innerHTML = '$'+`${price4}`;
-row.insertCell(3).innerHTML = ('$' + `${extended_price4}`);
-row = table.insertRow(); // Create a new row for each item
-row.insertCell(0).innerHTML = `${item5}`;
-row.insertCell(1).innerHTML = `${quantity5}`;
-row.insertCell(2).innerHTML = '$'+`${price5}`;
-row.insertCell(3).innerHTML = ('$' + `${extended_price5}`);
 
 // Calculate shipping based on sub-total
 let shippingCharge = 0;
@@ -80,6 +37,16 @@ if (subtotal <= 50) {
 
 // Calculate total including shipping
 total = subtotal + taxAmount + shippingCharge;
+
+// assumes there are no errors or logic requiremets on the display of data... which will not be true
+for (let i = 0; i < itemData.length; i++) {
+    let row = table.insertRow();
+    row.insertCell(0).innerHTML = itemData[i].brand;
+    row.insertCell(1).innerHTML = quantity[itemData[i].quantityIndex];
+    row.insertCell(2).innerHTML = '$' + itemData[i].price.toFixed(2);
+    row.insertCell(3).innerHTML = '$' + extendedPrices[i].toFixed(2);
+}
+
 
 // Set the total cell in bold
 document.getElementById('total_cell').innerHTML = `$${total.toFixed(2)}`;
